856bf74ede2b7a27eb65566924a2fe7c1aca8e7f
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-06-02 15:58:37 +0200

    Feat: create sign in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import e from "express";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,6 +15,39 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({
+      email,
+    });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials!"));
+    }
+
+    const token = jwt.sign(
+      { id: validUser._id, username: validUser.username },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...userInfo } = validUser._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
     next(error);
   }
